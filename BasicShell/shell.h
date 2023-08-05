4644817e72312806b8f95dacde44aaf684a222f3
bashthat <earthstar412@gmail.com>
2023-08-04 20:33:27 -0500

    Sea(C)_Shell

--- BasicShell/shell.h
+++ BasicShell/shell.h
@@ -25,6 +25,23 @@
 #define ANSI_COLOR_BLUE    "\x1b[34m"
 #define ANSI_COLOR_MAGENTA "\x1b[35m"
 #define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_WHITE   "\x1b[37m"
+#define ANSI_COLOR_BRIGHT_RED     "\x1b[91m"
+#define ANSI_COLOR_BRIGHT_GREEN   "\x1b[92m"
+#define ANSI_COLOR_BRIGHT_YELLOW  "\x1b[93m"
+#define ANSI_COLOR_BRIGHT_BLUE    "\x1b[94m"
+#define ANSI_COLOR_BRIGHT_MAGENTA "\x1b[95m"
+#define ANSI_COLOR_BRIGHT_CYAN    "\x1b[96m"
+#define ANSI_COLOR_BRIGHT_WHITE   "\x1b[97m"
+#define ANSI_COLOR_BRIGHT_RESET   "\x1b[0m"
+#define ANSI_COLOR_BRIGHT_BLACK   "\x1b[90m"
+#define ANSI_COLOR_BRIGHT_GRAY    "\x1b[37m"
+#define TOK_DELIM " \t\r\n\a"
+#define HISTORY_DELIM "\n"
+#define HISTORY_SIZE 100
+#define HISTORY_FILE ".myshell_history"
+#define HISTORY_FILE_PATH "/home/akshay/.myshell_history"
+#define TOK_BUFSIZE 64
 
 /* Functions and Prototypes */
 
--- BasicShell/shellFromScratch.c
+++ BasicShell/shellFromScratch.c
@@ -26,10 +26,6 @@
 // print the welcome message
 
 
-
-
-
-
 // parse the command and arguments
 
 void parse_command(char *command, char **args){
@@ -46,6 +42,168 @@ void parse_command(char *command, char **args){
     }
     *args = '\0';
 }
+
+// read_line function to read a line of input from stdin
+
+
+
+// exit status function
+
+int exit_status(void)
+{
+    int status;
+    if (WIFEXITED(status))
+    {
+        status = WEXITSTATUS(status);
+    }
+    return status;
+}
+
+// execute function to execute the command
+
+int execute(char **args)
+{
+    int i = 0;
+    if (args[0] == NULL)
+    {
+        // An empty command was entered.
+        return 1;
+    }
+    else if (strcmp(args[0], "exit") == 0)
+    {
+        // exit command
+        return 0;
+    }
+    else if (strcmp(args[0], "cd") == 0)
+    {
+        // cd command
+        if (args[1] == NULL)
+        {
+            fprintf(stderr, "myshell: expected argument to \"cd\"\n");
+        }
+        else
+        {
+            if (chdir(args[1]) != 0)
+            {
+                perror("myshell");
+            }
+        }
+        return 1;
+    }
+    else if (strcmp(args[0], "history") == 0)
+    {
+        // history command
+        FILE *fp;
+        fp = fopen("history.txt", "r");
+        char c;
+        c = fgetc(fp);
+        while (c != EOF)
+        {
+            printf("%c", c);
+            c = fgetc(fp);
+        }
+        fclose(fp);
+        return 1;
+    }
+    else
+    {
+        // fork() creates a new process
+        pid_t pid = fork();
+        if (pid == 0)
+        {
+            // child process
+            if (execvp(args[0], args) == -1)
+            {
+                perror("myshell");
+            }
+            exit(EXIT_FAILURE);
+        }
+        else if (pid < 0)
+        {
+            // error forking
+            perror("myshell");
+        }
+        else
+        {
+            // parent process
+            wait(NULL);
+        }
+        return 1;
+    }
+}
+
+// PATH handling
+
+void path_handling(char **args){
+    char *path = getenv("PATH");
+    char *path_tokens[MAX_NUM_PATHS];
+    int i = 0;
+    path_tokens[i] = strtok(path, ":");
+    while(path_tokens[i] != NULL){
+        i++;
+        path_tokens[i] = strtok(NULL, ":");
+    }
+    i = 0;
+    while(path_tokens[i] != NULL){
+        char *path = path_tokens[i];
+        strcat(path, "/");
+        strcat(path, args[0]);
+        execvp(path, args);
+        i++;
+    }
+}
+
+// Cat Command
+
+
+// History Command
+
+void history_command(char *command){
+    FILE *fp;
+    fp = fopen("history.txt", "a");
+    fprintf(fp, "%s\n", command);
+    fclose(fp);
+}
+
+// git add * command   
+
+void git_add_command(char *command){
+    FILE *fp;
+    fp = fopen("history.txt", "a");
+    fprintf(fp, "%s\n", command);
+    fclose(fp);
+
+    char *args[] = {"git", "add", "*", NULL};
+    pid_t pid = fork();
+
+    // if (pid == 0)
+    // {
+    //     // child process
+    //     if (execvp(args[0], args) == -1)
+    //     {
+    //         perror("myshell");
+    //     }
+    //     exit(EXIT_FAILURE);
+    // }
+    // else if (pid < 0)
+    // {
+    //     // error forking
+    //     perror("myshell");
+    // }
+    // else
+    // {
+    //     // parent process
+    //     wait(NULL);
+    // }
+    // return 1;
+
+    // git commit -m "message" command
+
+    // void git_commit(char *command){
+
+}
+
+
 
 
 int main(int argc, char **argv){
@@ -74,11 +232,53 @@ int main(int argc, char **argv){
         if (strcmp(command, "exit") == 0) {
             break;
         }
-
+                
         // clear the screen
 
         if (strcmp(command, "clear") == 0) {
             system("clear");
+            continue;
+        }
+
+        // change directory
+
+        if (strcmp(command, "cd") == 0) {
+            char *home = getenv("HOME");
+            chdir(home);
+            continue;
+        }
+
+        // change directory to specified directory
+
+        if (strncmp(command, "cd ", 3) == 0) {
+            char *path = command + 3;
+            chdir(path);
+            continue;
+        }
+
+
+        // history command
+
+        if (strcmp(command, "history") == 0) {
+            FILE *fp;
+            fp = fopen("history.txt", "r");
+            char c;
+            c = fgetc(fp);
+            while (c != EOF)
+            {
+                printf("%c", c);
+                c = fgetc(fp);
+            }
+            fclose(fp);
+            continue;
+        }
+
+        
+
+        // exit status
+
+        if (strcmp(command, "exit status") == 0) {
+            printf("%d\n", exit_status());
             continue;
         }
 
@@ -165,6 +365,55 @@ int main(int argc, char **argv){
 
         if (strcmp(command, "env") == 0) {
             system("env");
+            continue;
+        }
+
+        // set environment variables
+
+        if (strcmp(command, "setenv") == 0) {
+            system("set");
+            continue;
+        }
+
+        // print user id
+
+        if (strcmp(command, "id") == 0) {
+            system("id");
+            continue;
+        }
+
+        // USER environment variable
+
+        if (strcmp(command, "USER") == 0) {
+            system("USER");
+            continue;
+        }
+
+        // unset environment variables
+
+        if (strcmp(command, "unsetenv") == 0) {
+            system("unset");
+            continue;
+        }
+
+        // print current date and time
+
+        if (strcmp(command, "date") == 0) {
+            system("date");
+            continue;
+        }
+
+        // print current date and time in UTC
+
+        if (strcmp(command, "date -u") == 0) {
+            system("date -u");
+            continue;
+        }
+
+        // TAB completion
+
+        if (strcmp(command, "TAB") == 0) {
+            system("TAB");
             continue;
         }
 
